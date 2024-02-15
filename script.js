9907330b21f5c97e9117680498ef6f27c1cb03d4
Hasan Hacioglu <asanhix@gmail.com>
2024-02-15 21:35:30 +0300

    Add grid button

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
+const MAX_NUMBER_OF_GRID = 1600;
+let numberOfGrid = 1600
+
+// UIs
 function addElement(tagName, tagContent) {
     let element = document.createElement(tagName);
-    let content = document.createTextNode(tagContent)
+    let content = document.createTextNode(tagContent);
     element.appendChild(content);
     return element;
 }
 
-let container = addElement("div", "")
-container.classList.add("container")
-document.body.appendChild(container)
+let button = addElement("button", "Change Grid")
+document.body.appendChild(button)
+let container = addElement("div", "");
+container.classList.add("container");
+document.body.appendChild(container);
 
-
-for(let i = 0; i<16; i++) {
-    container.appendChild(addElement("div", ""))
+function makeGrids(){
+    for(let i = 0; i<numberOfGrid; i++) {
+        container.appendChild(addElement("div", ""));
+    }
+    
 }
+
+
+
+// Events
+
+container.addEventListener("mouseenter", function(e){
+    if(e.target.className != "container"){
+        e.target.classList.add("black-box");
+    }
+    
+},true)
+
+container.addEventListener("mouseleave", function(e){
+    if(e.target.className != "container"){
+        e.target.classList.add("pixaleted");
+    }
+},true)
+
+button.addEventListener("click", function(){
+    numberOfGrid = prompt("Please enter between 1-1600 grid", 1600)
+    while(container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+    makeGrids()
+})
