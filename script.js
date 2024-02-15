93ea63f615bd7dcacd3737f43cd451e87f09a472
Hasan Hacioglu <asanhix@gmail.com>
2024-02-15 16:20:16 +0300

    Add 16 grids

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function addElement(tagName, tagContent) {
     let element = document.createElement(tagName);
-    element.appendChild(tagContent);
+    let content = document.createTextNode(tagContent)
+    element.appendChild(content);
     return element;
 }
 
-let container = addElement("div", null)
+let container = addElement("div", "")
 container.classList.add("container")
 document.body.appendChild(container)
 
 
+for(let i = 0; i<16; i++) {
+    container.appendChild(addElement("div", ""))
+}
